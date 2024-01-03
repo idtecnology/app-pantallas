a99ff62668d6160f10c4ea4bf00d7482d84c03f0
Jesus Hernandez <jesus.hernandez@simpledatacorp.com>
2024-01-03 18:05:16 -0400

    Update pantalla2.blade.php

--- resources/views/pantalla2.blade.php
+++ resources/views/pantalla2.blade.php
@@ -218,7 +218,7 @@
                     }
 
                     media_container.innerHTML = add;
-                    window.location.reload(true);
+                    //window.location.reload(true);
 
                 })
                 .catch(error => {
